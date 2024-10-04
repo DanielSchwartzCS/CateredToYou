2307f008db4b384924d8977ae533c01005c7b4e3
Jamey Sheehy <jameys127@gmail.com>
2024-10-04 12:20:00 -0700

    you can now login through connecting to the database

--- app/src/main/java/com/example/cateredtoyou/LoginScreen.kt
+++ app/src/main/java/com/example/cateredtoyou/LoginScreen.kt
@@ -2,10 +2,16 @@ package com.example.cateredtoyou
 
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import android.widget.Button
 import android.widget.EditText
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import com.example.cateredtoyou.apifiles.DatabaseApi
+import com.example.cateredtoyou.apifiles.LoginResponse
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
 class LoginScreen : AppCompatActivity() {
 
@@ -21,15 +27,35 @@ class LoginScreen : AppCompatActivity() {
             val username = usernameEditText.text.toString()
             val password = passwordEditText.text.toString()
 
+            val call = DatabaseApi.retrofitService.loginCheck(username, password)
+            call.enqueue(object : Callback<LoginResponse> {
+                override fun onResponse(call: Call<LoginResponse>, response: Response<LoginResponse>){
+                    if(response.isSuccessful) {
+                        val rawResponse = response.body()
+                        val message = response.message().toString()
+                        if (rawResponse?.status == true) {
+                            val intent = Intent(this@LoginScreen, MainActivity::class.java)
+                            startActivity(intent)
+                            finish()
+                        } else {
+                            Toast.makeText(this@LoginScreen, response.message(), Toast.LENGTH_SHORT).show()
+                        }
+                    }
+                }
+
+                override fun onFailure(call: Call<LoginResponse>, t: Throwable) {
+                    Log.e("LoginScreen", "Failed to authorize user", t)
+                }
+            })
             // Dummy check for demonstration purposes
-            if (username == "admin" && password == "password") {
+//            if (username == "admin" && password == "password") {
                 // Go to MainActivity after successful login
-                val intent = Intent(this, MainActivity::class.java)
-                startActivity(intent)
-                finish()
-            } else {
-                Toast.makeText(this, "Invalid credentials", Toast.LENGTH_SHORT).show()
-            }
+//                val intent = Intent(this, MainActivity::class.java)
+//                startActivity(intent)
+//                finish()
+//            } else {
+//                Toast.makeText(this, "Invalid credentials", Toast.LENGTH_SHORT).show()
+//            }
         }
     }
 }
