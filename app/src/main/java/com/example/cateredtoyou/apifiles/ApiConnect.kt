a6c2453da0167631bcae047b1d52f594bb4b92d7
DanielSchwartzCS <98420686+DanielSchwartzCS@users.noreply.github.com>
2024-11-11 21:38:04 -0800

    pull apiconnect.kt from master

--- app/src/main/java/com/example/cateredtoyou/apifiles/ApiConnect.kt
+++ app/src/main/java/com/example/cateredtoyou/apifiles/ApiConnect.kt
@@ -1,46 +1,84 @@
 package com.example.cateredtoyou.apifiles
 
+import android.util.Log
 import com.example.cateredtoyou.Client
 import com.google.gson.GsonBuilder
 import com.google.gson.annotations.SerializedName
 import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 import retrofit2.http.GET
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 import retrofit2.http.Body
+import retrofit2.http.Field
+import retrofit2.http.FormUrlEncoded
 import retrofit2.http.Headers
 import retrofit2.http.POST
 import retrofit2.http.Query
 
+
 // Url for the AWS web server
 private const val BASE_URL = "http://ec2-13-56-230-200.us-west-1.compute.amazonaws.com/php/"
 
+// Lots of functions to connect to an api that returns a json file
+private val retrofit = Retrofit.Builder().addConverterFactory(GsonConverterFactory.create())
+    .baseUrl(BASE_URL)
+    .build()
 
-// Interface for all of the functions that call the API
+// interface for all of the functions that call the API
 interface ApiConnect {
+    @FormUrlEncoded
     @POST("login.php")
-    @Headers("Content-Type: application/json")
-    fun loginCheck(@Body loginRequest: LoginRequest): Call<LoginResponse>
+    fun loginCheck(
+        @Field("username") username: String,
+        @Field("password") password: String
 
+    ): Call<LoginResponse>
+
+
+    @FormUrlEncoded
     @POST("add_employee.php")
-    @Headers("Content-Type: application/json")
-    fun addUser(@Body addUserRequest: AddUserRequest): Call<AddUserResponse>
+    fun addUser(
+        @Field("username") username: String,
+        @Field("password") password: String
+
+    ): Call<AddUserResponse>
+
+
 
     @GET("get_employees.php")
     fun getUser(): Call<List<User>>
 
+    @FormUrlEncoded
     @POST("add_client.php")
-    @Headers("Content-Type: application/json")
-    fun addClient(@Body addClientRequest: AddClientRequest): Call<AddClientResponse>
+    fun addClient(
+        @Field("firstname") firstname: String,
+        @Field("lastname") lastname: String,
+        @Field("email") email : String,
+        @Field("phonenumber") phonenumber : String
+    ): Call<AddClientResponse>
 
     @GET("get_clients.php")
     fun getClient(): Call<List<Client>>
 
+    @FormUrlEncoded
     @POST("add_event.php")
-    @Headers("Content-Type: application/json")
-    fun addEvent(@Body addEventRequest: AddEventRequest): Call<EventResponse>
+    fun addEvent(
+        @Field("name") name: String,
+        @Field("event_date") eventDate: String,
+        @Field("event_start_time") startTime: String,
+        @Field("event_end_time") endTime: String,
+        @Field("location") location: String,
+        @Field("status") status: String,
+        @Field("number_of_guests") numberOfGuests: Int,
+        @Field("client_id") clientId: Int,
+        @Field("employee_id") employeeId: Int,
+        @Field("additional_info") additionalInfo: String
+    ): Call<EventResponse>
+
 
     @GET("get_events.php")
     fun getEvents(): Call<EventsResponse>
@@ -48,16 +86,22 @@ interface ApiConnect {
     @GET("get_inventory.php")
     fun getInventory(): Call<List<InventoryItem>>
 
+    @FormUrlEncoded
     @POST("add_event_inventory.php")
-    @Headers("Content-Type: application/json")
-    fun addEventInventory(@Body addEventInventoryRequest: AddEventInventoryRequest): Call<BaseResponse>
+    fun addEventInventory(
+        @Field("event_id") eventId: Int,
+        @Field("inventory_items") inventoryItems: String
+    ): Call<BaseResponse>
+
 
     @GET("get_event_inventory.php")
     fun getEventInventory(@Query("event_id") eventId: Int): Call<EventInventoryResponse>
 
     @POST("delete_event.php")
-    @Headers("Content-Type: application/json")
-    fun deleteEvent(@Body deleteEventRequest: DeleteEventRequest): Call<DeleteResponse>
+    @FormUrlEncoded
+    fun deleteEvent(
+        @Field("event_id") eventId: Int
+    ): Call<DeleteResponse>
 
     @GET("get_raw_inventory.php")
     fun getRawInventory(): Call<List<InventoryItem>>
@@ -65,9 +109,11 @@ interface ApiConnect {
     @POST("update_inventory.php")
     @Headers("Content-Type: application/json")
     fun updateInventory(@Body request: UpdateInventoryRequest): Call<BaseResponse>
+
+
 }
 
-// The connection object
+// the connection object
 object DatabaseApi {
     val retrofitService: ApiConnect by lazy {
         val logging = HttpLoggingInterceptor().apply {
@@ -91,49 +137,8 @@ object DatabaseApi {
             .create(ApiConnect::class.java)
     }
 }
+// Data classes for the different responses based on the call
 
-// Data classes for the different requests and responses
-
-data class LoginRequest(
-    val username: String,
-    val password: String
-)
-
-data class AddUserRequest(
-    val username: String,
-    val password: String
-)
-
-data class AddClientRequest(
-    val firstname: String,
-    val lastname: String,
-    val email: String,
-    val phonenumber: String
-)
-
-data class AddEventRequest(
-    val name: String,
-    val eventDate: String,
-    val startTime: String,
-    val endTime: String,
-    val location: String,
-    val status: String,
-    val numberOfGuests: Int,
-    val clientId: Int,
-    val employeeId: Int,
-    val additionalInfo: String
-)
-
-data class AddEventInventoryRequest(
-    val eventId: Int,
-    val inventoryItems: String
-)
-
-data class DeleteEventRequest(
-    val eventId: Int
-)
-
-// Existing response data classes
 data class AddUserResponse(
     val status: String,
     val message: String
@@ -146,8 +151,10 @@ data class User(
     val email: String? = null,
     val phone: String? = null,
     val role: String? = null,
-    @SerializedName("first_name") val firstName: String,
-    @SerializedName("last_name") val lastName: String
+    @SerializedName("first_name")
+    val firstName: String,
+    @SerializedName("last_name")
+    val lastName: String
 ) {
     override fun toString(): String = "$firstName $lastName ($role)"
 }
@@ -161,6 +168,58 @@ data class AddClientResponse(
     val status: Boolean,
     val message: String
 )
+
+fun clientCall(
+    onSuccess: (List<Client>) -> Unit,
+    onFailure: (Throwable) -> Unit = {t -> Log.e("ApiConnect", "Failed to connect", t)}
+){
+    DatabaseApi.retrofitService.getClient().enqueue(object : Callback<List<Client>> {
+        override fun onResponse(call: Call<List<Client>>, response: Response<List<Client>>) {
+            if (response.isSuccessful) {
+                val rawResponse = response.body() ?: emptyList()
+                onSuccess(rawResponse)
+            }else{
+                onFailure(Throwable("Failed to load clients: ${response.message()}"))
+            }
+        }
+
+        override fun onFailure(call: Call<List<Client>>, t: Throwable) {
+            onFailure(t)
+        }
+    })
+}
+fun addClient(
+    firstname: String,
+    lastname: String,
+    email: String,
+    phonenumber: String,
+    onSuccess: (AddClientResponse) -> Unit,
+    onPartialSuccess: (AddClientResponse) -> Unit,
+    onFailure: (Throwable) -> Unit
+){
+    DatabaseApi.retrofitService.addClient(firstname, lastname, email, phonenumber).enqueue(object : Callback<AddClientResponse>{
+        override fun onResponse(
+            call: Call<AddClientResponse>,
+            response: Response<AddClientResponse>
+        ) {
+            if(response.isSuccessful){
+                val rawResponse = response.body()
+                if (rawResponse != null && rawResponse.status) {
+                    onSuccess(rawResponse)
+                }else if(rawResponse != null){
+                    onPartialSuccess(rawResponse)
+                }
+            }else{
+                onFailure(Throwable("Failed to add client: ${response.message()}"))
+            }
+        }
+
+        override fun onFailure(call: Call<AddClientResponse>, t: Throwable) {
+            onFailure(t)
+        }
+    })
+
+}
 
 data class EventResponse(
     @SerializedName("status") val status: Boolean,
@@ -211,6 +270,7 @@ data class InventoryItem(
     override fun toString(): String = itemName
 }
 
+
 data class BaseResponse(
     val status: Boolean,
     val message: String
@@ -229,12 +289,10 @@ data class EventInventoryItem(
     val category: String?,
     val unitOfMeasurement: String?
 )
-
 data class DeleteResponse(
     val status: Boolean,
     val message: String
 )
-
 data class UpdateInventoryRequest(
     @SerializedName("id") val id: Int,
     @SerializedName("quantity") val quantity: Int
