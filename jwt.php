d2ec5faeb7e92078b4aaf078310c86ea911e045b
EC2 Default User <ec2-user@ip-172-31-28-216.us-west-1.compute.internal>
2024-11-18 06:59:34 +0000

    initial commit

---
+++ jwt.php
@@ -0,0 +1,45 @@
+<?php
+
+require_once 'response.php';
+
+require_once __DIR__ . '/vendor/autoload.php';
+
+use \Firebase\JWT\JWT;
+
+// Function to generate a JWT token
+function generateJwt($userId, $role) {
+    $secretKey = "secret_key"; // TODO: make this an env var
+    $issuedAt = time();
+    $expirationTime = $issuedAt + 3600; // JWT expiration time
+
+    $payload = [
+        'iat' => $issuedAt,
+        'exp' => $expirationTime,
+        'userId' => $userId,
+        'role' => $role
+    ];
+    try {
+        echo "Payload: ";
+        var_dump($payload);
+	$tokenResponse = JWT::encode($payload, $secretKey, 'HS256');
+	echo "generated";
+        return $tokenResponse;
+    } catch (Exception $e) {
+        // Log the error for debugging
+        echo("Error generating JWT: " . $e->getMessage());
+        return null;
+    }
+
+}
+
+// Function to validate a JWT token
+function validateJwt($jwt) {
+    $secretKey = "secret_key"; // TODO: make this an env var
+    try {
+        $decoded = JWT::decode($jwt, $secretKey, ['HS256']);
+        return $decoded;
+    } catch (Exception $e) {
+        respondWithError("Invalid token: " . $e->getMessage(), 401);
+    }
+}
+?>
---
+++ vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'name' => 'cty/php',
+        'pretty_version' => '1.0.0+no-version-set',
+        'version' => '1.0.0.0',
+        'reference' => null,
+        'type' => 'project',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'cty/php' => array(
+            'pretty_version' => '1.0.0+no-version-set',
+            'version' => '1.0.0.0',
+            'reference' => null,
+            'type' => 'project',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'firebase/php-jwt' => array(
+            'pretty_version' => 'v6.10.1',
+            'version' => '6.10.1.0',
+            'reference' => '500501c2ce893c824c801da135d02661199f60c5',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../firebase/php-jwt',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
